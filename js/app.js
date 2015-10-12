7b4742a427156d9147ce8c1a8a6517dbf772fed8
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-10-12 04:21:27 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-var circle = new Circle('.drop1 li', {
-	angleX:        100,
-    angleY:        100,
-});
+
+
+
+window.onload = function() {
+	// body...
+	var circle = new Circle('.drop1 li', {
+		angleX:        100,
+	    angleY:        100,
+	});
+
+	var circle = new Circle('.drop2 li', {
+		angleX:        100,
+	    angleY:        100,
+	});
+}
